edc8724eeabcab8e349ea491468b508ceb230ea4
Koshin <koshin.1116@gmail.com>
2022-03-26 21:56:43 +0800

    feat: add LifecycleAccessibilityService

---
+++ sugar/src/main/java/com/github/foodiestudio/sugar/accessibility/LifecycleAccessibilityService.kt
@@ -0,0 +1,36 @@
+package com.github.foodiestudio.sugar.accessibility
+
+import android.accessibilityservice.AccessibilityService
+import android.view.accessibility.AccessibilityEvent
+import androidx.annotation.CallSuper
+import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.LifecycleOwner
+import androidx.lifecycle.ServiceLifecycleDispatcher
+
+/**
+ * A AccessibilityService that is also a {@link LifecycleOwner}.
+ */
+abstract class LifecycleAccessibilityService : AccessibilityService(), LifecycleOwner {
+    private val mDispatcher = ServiceLifecycleDispatcher(this)
+
+    @CallSuper
+    override fun onCreate() {
+        mDispatcher.onServicePreSuperOnCreate()
+        super.onCreate()
+    }
+
+    @CallSuper
+    override fun onServiceConnected() {
+        mDispatcher.onServicePreSuperOnBind()
+        super.onServiceConnected()
+    }
+
+    @CallSuper
+    override fun onDestroy() {
+        mDispatcher.onServicePreSuperOnDestroy()
+        super.onDestroy()
+    }
+
+    override fun getLifecycle(): Lifecycle = mDispatcher.lifecycle
+
+}
