9b6fd6a6af99089d7882157db5f572a7a970d53a
Jiangc <jiangc@vistring.com>
2022-07-20 16:08:53 +0800

    feat: add FooterItemDecoration

--- sugar/src/main/java/com/github/foodiestudio/sugar/recyclerView/FooterItemDecoration.kt
+++ sugar/src/main/java/com/github/foodiestudio/sugar/recyclerView/FooterItemDecoration.kt
@@ -9,9 +9,9 @@ import androidx.recyclerview.widget.RecyclerView
 /**
  * 适用于绘制分类的头部，仅当 [shouldShowHeader] 情况满足的情况下才会展示 [header]
  */
-internal class HeaderItemDecoration(
-    private val header: View,
-    private val shouldShowHeader: (Int) -> Boolean
+internal class FooterItemDecoration(
+    private val footer: View,
+    private val shouldShowFooter: (Int) -> Boolean
 ) : RecyclerView.ItemDecoration() {
     override fun getItemOffsets(
         outRect: Rect,
@@ -21,27 +21,27 @@ internal class HeaderItemDecoration(
     ) {
         super.getItemOffsets(outRect, view, parent, state)
         val itemPosition = parent.getChildAdapterPosition(view)
-        if (shouldShowHeader(itemPosition)) {
-            header.measure(
+        if (shouldShowFooter(itemPosition)) {
+            footer.measure(
                 View.MeasureSpec.makeMeasureSpec(parent.measuredWidth, View.MeasureSpec.EXACTLY),
                 View.MeasureSpec.makeMeasureSpec(
                     View.MeasureSpec.UNSPECIFIED,
                     View.MeasureSpec.UNSPECIFIED
                 )
             )
-            outRect.top = header.measuredHeight
+            outRect.bottom = footer.measuredHeight
         }
     }
 
     override fun onDraw(c: Canvas, parent: RecyclerView, state: RecyclerView.State) {
         super.onDraw(c, parent, state)
-        header.layout(parent.left, 0, parent.right, header.measuredHeight)
+        footer.layout(parent.left, 0, parent.right, footer.measuredHeight)
         parent.children.forEach {
             val position = parent.getChildAdapterPosition(it)
-            if (position != RecyclerView.NO_POSITION && shouldShowHeader(position)) {
+            if (position != RecyclerView.NO_POSITION && shouldShowFooter(position)) {
                 c.save()
-                c.translate(0f, it.y - header.measuredHeight)
-                header.draw(c)
+                c.translate(0f, it.y + it.measuredHeight)
+                footer.draw(c)
                 c.restore()
             }
         }
