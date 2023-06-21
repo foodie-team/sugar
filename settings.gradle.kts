765667df54a0f1ed6073c39aa09417d786d2238f
Jiangc <jiangc@vistring.com>
2023-06-21 18:07:54 +0800

    feat: use shared libs versions plugin

---
+++ settings.gradle.kts
@@ -0,0 +1,33 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+        maven {
+            name = "Github Packages"
+            url = uri("https://maven.pkg.github.com/foodiestudio/libs-versions")
+            credentials {
+                // your github username
+                username = extra["github.username"] as String
+                // https://github.com/settings/tokens
+                password = extra["github.token"] as String
+            }
+        }
+    }
+    versionCatalogs {
+        create("sharedLibs") {
+            from("com.github.foodiestudio:libs-versions:2023.01.00-rc.3")
+        }
+    }
+}
+
+rootProject.name = "Sugar"
+include(":sugar")
