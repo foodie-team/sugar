668a647528f49d99818e786e51a57327baa2cb57
Jiangc <jiangc@vistring.com>
2023-06-26 11:47:58 +0800

    refactor: use shared versions catalog

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -11,20 +11,10 @@ dependencyResolutionManagement {
     repositories {
         google()
         mavenCentral()
-        maven {
-            name = "Github Packages"
-            url = uri("https://maven.pkg.github.com/foodiestudio/libs-versions")
-            credentials {
-                // your GitHub username
-                username = extra["github.username"] as String
-                // https://github.com/settings/tokens
-                password = extra["github.token"] as String
-            }
-        }
     }
     versionCatalogs {
         create("sharedLibs") {
-            from("com.github.foodiestudio:libs-versions:2023.01.00")
+            from("io.github.foodiestudio:libs-versions:2023.01.00")
             version("lifecycle", "2.6.1")
         }
     }
