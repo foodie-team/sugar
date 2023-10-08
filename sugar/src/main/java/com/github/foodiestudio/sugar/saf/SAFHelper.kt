60d2665475e585f4adc510e17249c14d5e453168
KoShin <koshin.1116@gmail.com>
2023-10-08 22:16:43 +0800

    refactor: make fileSystem public

--- sugar/src/main/java/com/github/foodiestudio/sugar/saf/SAFHelper.kt
+++ sugar/src/main/java/com/github/foodiestudio/sugar/saf/SAFHelper.kt
@@ -12,11 +12,9 @@ import com.google.modernstorage.storage.toOkioPath
 import okio.FileHandle
 import okio.FileMetadata
 import okio.FileSystem
-import okio.IOException
+import okio.ForwardingFileSystem
 import okio.Path
 import okio.Path.Companion.toOkioPath
-import okio.Sink
-import okio.Source
 import java.io.File
 
 /**
@@ -24,8 +22,9 @@ import java.io.File
  */
 @SuppressLint("StaticFieldLeak")
 @ExperimentalSugarApi
-object SAFHelper : FileSystem() {
-    internal lateinit var fileSystem: AndroidFileSystem
+object SAFHelper {
+    // okio 定义的 FileSystem 接口
+    lateinit var fileSystem: FileSystem
 
     internal lateinit var storagePermissions: StoragePermissions
 
@@ -36,83 +35,42 @@ object SAFHelper : FileSystem() {
      */
     fun init(context: Context) {
         val appContext = context.applicationContext
-        fileSystem = AndroidFileSystem(appContext)
         storagePermissions = StoragePermissions(appContext)
         contentResolver = appContext.contentResolver
-    }
+        // workaround: AndroidFileSystem 没有实现的几个方法使用 SYSTEM 实现
+        fileSystem = object : ForwardingFileSystem(AndroidFileSystem(appContext)) {
+            override fun createDirectory(dir: Path, mustCreate: Boolean) =
+                SYSTEM.createDirectory(dir, mustCreate)
 
-    override fun copy(source: Path, target: Path) {
-        fileSystem.copy(source = source, target = target)
-    }
+            override fun atomicMove(source: Path, target: Path) = SYSTEM.atomicMove(source, target)
 
-    /**
-     * get metadata, for example:
-     * ```
-     * displayName: extra(DisplayName::class).value
-     * mimeType: extra(MimeType::class).value
-     * ```
-     */
-    override fun metadataOrNull(path: Path): FileMetadata? = fileSystem.metadataOrNull(path)
+            override fun openReadOnly(file: Path): FileHandle = SYSTEM.openReadOnly(file)
 
-    override fun appendingSink(file: Path, mustExist: Boolean): Sink =
-        fileSystem.appendingSink(file, mustExist)
-
-    // TODO:
-    override fun atomicMove(source: Path, target: Path) = fileSystem.atomicMove(source, target)
-
-    override fun canonicalize(path: Path): Path = fileSystem.canonicalize(path)
-
-    override fun createDirectory(dir: Path, mustCreate: Boolean) {
-        if (!dir.toFile().mkdir()) {
-            val alreadyExist = metadataOrNull(dir)?.isDirectory == true
-            if (alreadyExist) {
-                if (mustCreate) {
-                    throw IOException("$dir already exist.")
-                } else {
-                    return
-                }
-            }
-            throw IOException("failed to create directory: $dir")
+            override fun openReadWrite(
+                file: Path,
+                mustCreate: Boolean,
+                mustExist: Boolean
+            ): FileHandle = SYSTEM.openReadWrite(file, mustCreate, mustExist)
         }
     }
-
-    override fun createSymlink(source: Path, target: Path) =
-        fileSystem.createSymlink(source, target)
-
-    override fun delete(path: Path, mustExist: Boolean) = fileSystem.delete(path, mustExist)
-
-    override fun list(dir: Path): List<Path> = fileSystem.list(dir)
-
-    override fun listOrNull(dir: Path): List<Path>? = fileSystem.listOrNull(dir)
-
-    // TODO:
-    override fun openReadOnly(file: Path): FileHandle = fileSystem.openReadOnly(file)
-
-    // TODO:
-    override fun openReadWrite(file: Path, mustCreate: Boolean, mustExist: Boolean): FileHandle =
-        fileSystem.openReadWrite(file, mustCreate, mustExist)
-
-    override fun sink(file: Path, mustCreate: Boolean): Sink = fileSystem.sink(file, mustCreate)
-
-    override fun source(file: Path): Source = fileSystem.source(file)
 }
 
 @ExperimentalSugarApi
-fun SAFHelper.copy(source: Uri, dest: File) {
-    copy(source.toOkioPath(), dest.toOkioPath())
-}
+fun SAFHelper.copy(source: Uri, dest: File) =
+    fileSystem.copy(source.toOkioPath(), dest.toOkioPath())
 
 @ExperimentalSugarApi
-fun SAFHelper.metadataOrNull(uri: Uri): FileMetadata? = metadataOrNull(uri.toOkioPath())
+fun SAFHelper.metadataOrNull(uri: Uri): FileMetadata? = fileSystem.metadataOrNull(uri.toOkioPath())
 
 /**
  * 查询 [uri] 对应的 MimeType，查询不到的情况为返回 null
  * 不同 Android 版本所能识别的类型是不同的，例如: 字体文件 `font/ttf` 在低版本会被当作 `application/octet-stream`
  */
 @ExperimentalSugarApi
-fun SAFHelper.mimeTypeOrNull(uri: Uri): String? = metadataOrNull(uri.toOkioPath())?.extra(
-    MetadataExtras.MimeType::class
-)?.value
+fun SAFHelper.mimeTypeOrNull(uri: Uri): String? =
+    fileSystem.metadataOrNull(uri.toOkioPath())?.extra(
+        MetadataExtras.MimeType::class
+    )?.value
 
 /**
  * 查询 [uri] 对应的文件绝对路经，查询不到的情况为返回 null
@@ -124,11 +82,12 @@ fun SAFHelper.mimeTypeOrNull(uri: Uri): String? = metadataOrNull(uri.toOkioPath(
  *
  * Scoped Storage 的机制也是希望保护用户的隐私，避免只是访问 File Path，取而代之的是 DocumentFile
  */
-@OptIn(ExperimentalSugarApi::class)
+@ExperimentalSugarApi
 @Deprecated("scoped storage 基本上是拿不到路径的，请使用 DocumentFile 来代替")
-fun SAFHelper.absoluteFilePathOrNull(uri: Uri): String? = metadataOrNull(uri.toOkioPath())?.extra(
-    MetadataExtras.FilePath::class
-)?.value
+fun SAFHelper.absoluteFilePathOrNull(uri: Uri): String? =
+    fileSystem.metadataOrNull(uri.toOkioPath())?.extra(
+        MetadataExtras.FilePath::class
+    )?.value
 
 /**
  * 查询 [uri] 对应的文件名，查询不到的情况为返回 null
@@ -136,4 +95,4 @@ fun SAFHelper.absoluteFilePathOrNull(uri: Uri): String? = metadataOrNull(uri.toO
  */
 @ExperimentalSugarApi
 fun SAFHelper.displayNameOrNull(uri: Uri): String? =
-    metadataOrNull(uri.toOkioPath())?.extra(MetadataExtras.DisplayName::class)?.value
+    fileSystem.metadataOrNull(uri.toOkioPath())?.extra(MetadataExtras.DisplayName::class)?.value
